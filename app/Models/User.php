fa74ba3a2a880668802100b8e2ed599af3b14a89
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-04-14 18:27:28 +0200

    generated jwt secret and defined functions for the token in model users

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    //@return array
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
 }
